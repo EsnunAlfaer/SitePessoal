ac1c2967018b827680a6baecd9484023ac389809
EsnunAlfaer <rnbarbosa02@gmail.com>
2022-11-29 19:39:03 -0300

    ponto

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ function Home() {
         <body> 
             <div>
                 Não tem nada aqui, ainda...
-                <img>https://www.google.com/imgres?imgurl=https%3A%2F%2Fs2.glbimg.com%2FatIp_axsQ9CQci2-CQ6zGIqLTUU%3D%2F1200x%2Fsmart%2Ffilters%3Acover()%3Astrip_icc()%2Fs.glbimg.com%2Fes%2Fge%2Ff%2Foriginal%2F2014%2F11%2F20%2Fabacate.jpg&imgrefurl=https%3A%2F%2Fge.globo.com%2Feu-atleta%2Fnutricao%2Fnoticia%2Fabacate-engorda-beneficios-maleficios-e-tabela-nutricional-do-fruto-rico-em-omega-9.ghtml&tbnid=9kfetlMA7JmWaM&vet=12ahUKEwii8PyGutT7AhXjs5UCHdx8Cw0QMygAegUIARDoAQ..i&docid=uaJoNVWyZJ_huM&w=1200&h=1002&q=abacate&client=firefox-b-d&ved=2ahUKEwii8PyGutT7AhXjs5UCHdx8Cw0QMygAegUIARDoAQ</img>
+                <img>https://s2.glbimg.com/atIp_axsQ9CQci2-CQ6zGIqLTUU=/1200x/smart/filters:cover():strip_icc()/s.glbimg.com/es/ge/f/original/2014/11/20/abacate.jpg</img>
             </div>
         </body>
     </html>  
